73e92cd5a5f4facdda373220b537f776325782c7
Sungwon Lee <sung1lee@kaist.ac.kr>
2024-04-11 11:31:56 +0000

    refactor: 폴더 구조 수정

--- app/backend/src/main/java/com/microbet/MicrobetApplication.java
+++ app/backend/src/main/java/com/microbet/MicrobetApplication.java
@@ -1,4 +1,4 @@
-package com.microbet.microbet;
+package com.microbet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- app/backend/src/test/java/com/microbet/MicrobetApplicationTests.java
+++ app/backend/src/test/java/com/microbet/MicrobetApplicationTests.java
@@ -1,4 +1,4 @@
-package com.microbet.microbet;
+package com.microbet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
