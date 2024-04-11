bfe23efb17e83527a95bb57760dde0eeb6c13ed1
Sungwon Lee <sung1lee@kaist.ac.kr>
2024-04-11 11:25:04 +0000

    feat: 폴더구조 및 bolierplate initailize

---
+++ app/backend/src/main/java/com/microbet/microbet/MicrobetApplication.java
@@ -0,0 +1,13 @@
+package com.microbet.microbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobetApplication.class, args);
+	}
+
+}
---
+++ app/backend/src/test/java/com/microbet/microbet/MicrobetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microbet.microbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
